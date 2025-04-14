4008b77f04521aeffadee9037272cb928cdbe294
adittrp <166170305+adittrp@users.noreply.github.com>
2025-04-14 13:16:53 -0500

    s
    s

---
+++ scripts/bullet.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@export var speed := 800.0
+var direction: Vector2 = Vector2.ZERO
+
+func _physics_process(delta):
+	position += direction * speed * delta
+
+func _ready():
+	await get_tree().create_timer(3.0).timeout
+	queue_free()
+
+func _on_area_entered(area: Area2D) -> void:
+	queue_free()
+	print("ARWA")
--- scripts/player.gd
+++ scripts/player.gd
@@ -6,14 +6,36 @@ signal died
 @onready var camera_remote_transform = $CameraRemoteTransform
 
 
+var bullet_scene = preload("res://scenes/bullet.tscn")
+var canShoot = true
 
 func _process(delta):
 	look_at(get_global_mouse_position())
 	if Input.is_action_just_pressed("quit"):
-		get_tree().quit()		
+		get_tree().quit()
+		
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and canShoot:
+		shoot()
+		canShoot = false
+		await get_tree().create_timer(0.25).timeout
+		canShoot = true
 	
-@export var speed := 500.0  # Movement speed
+@export var speed := 500.0
 
+func shoot():
+	var bullet = bullet_scene.instantiate()
+	bullet.global_position = global_position
+	
+	var shoot_direction = (get_global_mouse_position() - global_position).normalized()
+	
+	var spawn_offset = shoot_direction * 30  # Tune this distance as needed
+	bullet.global_position = global_position + spawn_offset
+	
+	bullet.direction = shoot_direction
+	bullet.rotation = shoot_direction.angle()
+	
+	get_tree().current_scene.add_child(bullet)
+	
 func _physics_process(delta):
 	var direction := Vector2.ZERO
 	
