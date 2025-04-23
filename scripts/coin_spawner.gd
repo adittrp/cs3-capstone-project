d896bb3d73ae12aab954d31a6f257bf41044b56a
sanjay-codes <sanjaykrishnan2026@gmail.com>
2025-04-22 19:13:26 -0500

    coins spawner stuff

---
+++ scripts/coin_spawner.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@export var coin_scene: PackedScene
+@export var coin_count: int = 15
+@export var player: Node2D
+@export var spawn_radius: float = 10000
+
+func _ready():
+	randomize()
+	for i in coin_count:
+		var coin = coin_scene.instantiate()
+		var angle = randf() * TAU
+		var radius = randf() * spawn_radius
+		var offset = Vector2(cos(angle), sin(angle)) * radius
+		coin.position = player.global_position + offset
+		add_child(coin)
