78c89f8d38a9701ea4129f240b84c69fedfbeca1
adittrp <166170305+adittrp@users.noreply.github.com>
2025-05-06 12:49:39 -0500

    Extra level stuff

--- scenes/enemy_manager.gd
+++ scenes/enemy_manager.gd
@@ -1,30 +1,30 @@
 extends Node2D
 
 var roundSystem = {
-	"Round1": [["Zombie", 50, 60]],
-	"Round2": [],
-	"Round3": [],
-	"Round4": [],
-	"Round5": []
+	"Level1": [["Zombie", 50, 60]],
+	"Level2": [],
+	"Level3": [],
+	"Level4": [],
+	"Level5": []
 }
-var roundNumber : int = 1
+var LevelNumber : int = 1
 
 @onready var enemy = load("res://scenes/enemy.tscn")
 @onready var player = get_parent().get_node("Player") # Adjust path if needed
 
 func _ready():
 	await get_tree().create_timer(1).timeout
-	startRound(roundNumber)
+	startRound(LevelNumber)
 	
-func startRound(round):
+func startRound(level):
 	await get_tree().create_timer(1).timeout
 	
 	
-	if round < 1:
+	if level < 1:
 		print("Invalid round number")
 		return
 	
-	var roundData = roundSystem["Round" + str(round)]
+	var roundData = roundSystem["Level" + str(round)]
 	
 	for enemy_info in roundData:
 		var count_per_wave = enemy_info[1]
@@ -40,7 +40,7 @@ func spawn_enemies_around_player(count: int):
 		var new_enemy = enemy.instantiate()
 		
 		var angle = randf_range(0, TAU)
-		var distance = 1500
+		var distance = 1750
 		var offset = Vector2(cos(angle), sin(angle)) * distance
 		new_enemy.position = player.global_position + offset
 		
